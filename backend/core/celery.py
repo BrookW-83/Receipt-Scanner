8a28fe95c127ebf2215945738be2604a8a55fbc8
Brook  Worku Alemu <109297876+BrookW-83@users.noreply.github.com>
2026-03-27 00:19:04 +0300

    fix: Change process queing error, update celery to send receipt scanner to correct process

--- backend/core/celery.py
+++ backend/core/celery.py
@@ -6,3 +6,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 app = Celery('core')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+# Route receipt_scanner tasks to dedicated queue
+app.conf.task_routes = {
+    'receipt_scanner.tasks.*': {'queue': 'receipt_scanner'},
+}
